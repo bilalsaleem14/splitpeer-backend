0591b948dcf02663fd1dc97ac2a91503b25a4f72
hassanarif2811@gmail.com <hassanarif2811@gmail.com>
2025-12-11 14:22:30 +0500

    Updated custom social adaptor to set user's fullname.

--- api/sso/adaptor.py
+++ api/sso/adaptor.py
@@ -19,6 +19,12 @@ def extract_picture_url(extra):
 
 
 class CustomSocialAdapter(DefaultSocialAccountAdapter):
+
+    def populate_user(self, request, sociallogin, data):
+        user = super().populate_user(request, sociallogin, data)
+        fullname = (data.get("name") or f"{data.get('first_name', '')} {data.get('last_name', '')}".strip() or "social user")
+        user.fullname = fullname
+        return user
     
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
